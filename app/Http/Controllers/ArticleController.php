2b9fb84533c2714fbb8b7c675fa799df217432a8
André Castelo <andrecastelo@gmail.com>
2017-04-01 18:30:00 -0300

    added article controller

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        return Article::all();
+    }
+
+    public function show(Article $article)
+    {
+        return $article;
+    }
+}

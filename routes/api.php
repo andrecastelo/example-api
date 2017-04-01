f7e4340485dff2419e37bc960a0c3919a356dfa1
André Castelo <andrecastelo@gmail.com>
2017-04-01 18:31:05 -0300

    article routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/{article}', 'ArticleController@show');

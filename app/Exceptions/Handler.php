53f6099f30d0a2c5bdea8b4e6f7390766f17b8c9
André Castelo <andrecastelo@gmail.com>
2017-04-29 20:39:30 -0300

    changed exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -67,10 +67,6 @@ class Handler extends ExceptionHandler
      */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
-
-        return redirect()->guest(route('login'));
+        return response()->json(['error' => 'Unauthenticated.'], 401);
     }
 }

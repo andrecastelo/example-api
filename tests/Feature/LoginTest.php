d7207206938ca2c71bd4d418540351afcbcae546
André Castelo <andrecastelo@gmail.com>
2017-04-30 01:23:49 -0300

    added testing and testing configuration

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class LoginTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testRequiresEmailAndLogin()
+    {
+        $this->json('POST', 'api/login')
+            ->assertStatus(422)
+            ->assertJson([
+                'email' => [ 'The email field is required.' ],
+                'password' => [ 'The password field is required.' ],
+            ]);
+    }
+
+    public function testUserLoginsSuccessfully()
+    {
+        $this->json('POST', 'api/login', ['email' => 'admin@test.com', 'password' => 'toptal'])
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    'id',
+                    'name',
+                    'email',
+                    'created_at',
+                    'updated_at',
+                    'api_token',
+                ],
+            ]);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,17 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, DatabaseMigrations;
+
+    public function setUp()
+    {
+        parent::setUp();
+        Artisan::call('db:seed');
+    }
 }
